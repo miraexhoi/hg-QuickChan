4f705d9b4d798bed609bede0d9f12735ea7ffb69
최미래 <miraechoi@dgsw.hs.kr>
2024-08-26 03:18:13 +0900

    feature :: add exception

---
+++ source_codes/spring_boot_server/Main/src/main/java/com/example/main/global/exception/BusinessException.java
@@ -0,0 +1,11 @@
+package com.example.main.global.exception;
+
+import com.example.main.global.exception.error.ErrorProperty;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class BusinessException extends RuntimeException {
+    private final ErrorProperty error;
+}
