c357919b4e5cd07d74bbb2a8f7993d65c836f770
nanaring1 <166397141+nanaring1@users.noreply.github.com>
2024-08-26 02:06:46 +0900

    Update App.js

--- source_codes/helper_app/booreung/src/App.js
+++ source_codes/helper_app/booreung/src/App.js
@@ -1,9 +1,25 @@
-import './App.css';
-import Router from './component/router.jsx'
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import StartPage from './components/StartPage';
+import SignupStep1 from './components/SignupStep1';
+import SignupStep2 from './components/SignupStep2';
+import SignupStep3 from './components/SignupStep3';
+import SignupStep4 from './components/SignupStep4';
+import LoginPage from './components/LoginPage';
 
 function App() {
   return (
-    <Router />
+    <Router>
+      <Routes>
+        <Route path="/" element={<StartPage />} />
+        <Route path="/signup-step1" element={<SignupStep1 />} />
+        <Route path="/signup-step2" element={<SignupStep2 />} />
+        <Route path="/signup-step3" element={<SignupStep3 />} />
+        <Route path="/signup-step4" element={<SignupStep4 />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </Router>
   );
 }
 
